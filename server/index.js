6f9defd0a42c24683954f497967469f994ef02e1
Diwakar Bhagat <diwakarbhagat.pro@gmail.com>
2026-02-25 03:43:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 const normalizeOrigin = (value) => value.replace(/\/+$/, "");
 const defaultOrigins = [
-    "https://ox-client.onrender.com",
+    "https://diwakarbhagathireox.onrender.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ];
